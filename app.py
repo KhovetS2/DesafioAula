9b14f8115755c1c9ba6d449869524e5d6ef0284e
KhovetS2 <isaque.elis.escola@gmail.com>
2022-09-07 11:40:18 -0300

    first commit

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template("index.html")
+
+@app.route('/contatos')
+def contatos():
+    return render_template("contatos.html")
+
+@app.route('/quem-somos')
+def quem_somos():
+    return render_template("quem-somos.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
